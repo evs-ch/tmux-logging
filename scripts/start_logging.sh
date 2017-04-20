ce4f47482b2024289924d761671dbd390edf1ca5
evs-ch <evs-ch@users.noreply.github.com>
2017-04-20 14:16:51 +0200

    disable filtering

--- scripts/start_logging.sh
+++ scripts/start_logging.sh
@@ -15,6 +15,11 @@ pipe_pane_ansifilter() {
 	tmux pipe-pane "exec cat - | ansifilter >> $FILE"
 }
 
+
+pipe_pane_unfiltered() {
+	tmux pipe-pane "exec cat - >> $FILE"
+}
+
 pipe_pane_sed_osx() {
 	# Warning, very complex regex ahead.
 	# Some characters below might not be visible from github web view.
@@ -28,14 +33,15 @@ pipe_pane_sed() {
 }
 
 start_pipe_pane() {
-	if ansifilter_installed; then
-		pipe_pane_ansifilter
-	elif system_osx; then
-		# OSX uses sed '-E' flag and a slightly different regex
-		pipe_pane_sed_osx
-	else
-		pipe_pane_sed
-	fi
+	pipe_pane_unfiltered
+	# if ansifilter_installed; then
+		# pipe_pane_ansifilter
+	# elif system_osx; then
+		# # OSX uses sed '-E' flag and a slightly different regex
+		# pipe_pane_sed_osx
+	# else
+		# pipe_pane_sed
+	# fi
 }
 
 main() {
